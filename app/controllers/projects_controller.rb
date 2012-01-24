9c44cd88082e780fbdc6d2ea3e8644a740498606
Ivan Storck <ivanoats@gmail.com>
2012-01-23 21:27:09 -0800

    p. 81 validations done

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -9,11 +9,15 @@ class ProjectsController < ApplicationController
 
   def create
     @project = Project.new(params[:project])
-    @project.save
-    flash[:notice] = "Project has been created."
-    redirect_to @project
+    if @project.save
+      flash[:notice] = "Project has been created."
+      redirect_to @project
+    else
+      flash[:notice] = "Project has not been created."
+      render action: "new" 
+    end
   end
-  
+
   def show
     @project = Project.find(params[:id])
   end
